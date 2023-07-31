1bb8768506e1852d0f57376844c3e4e3d6274c14
Brandon <brandonmurry168@gmail.com>
2023-07-30 21:24:39 -0400

    Added test using the players rootpart

--- main.lua
+++ main.lua
@@ -1,1 +1,6 @@
-local player = game:GetService("Players")
+local player: Player = game:GetService("Players").LocalPlayer
+repeat wait() until player.Character
+local character = player.Character
+local humanoid = character:FindFirstChild("Humanoid")
+
+print(humanoid.RootPart.Position)
