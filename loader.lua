5ac1b5605fe0e9c5e0a8fddca492b4722c652475
Brandon <brandonmurry168@gmail.com>
2023-08-03 00:16:35 -0400

    new commit

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
-if game.PlaceId == "8304191830" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/KIunk/rockethub/main/main.lua"))()
+if game.PlaceId == 8304191830 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/KIunk/rockethub/main/AnimeAdventures.lua"))()
 
 else
     print("Wrong game bucko!")
