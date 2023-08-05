be2e9419c322871d01457d8594288843a138fd0f
Brandon <brandonmurry168@gmail.com>
2023-08-04 20:25:14 -0400

    Added create folder for RocketHub if not exists

--- AnimeAdventures.lua
+++ AnimeAdventures.lua
@@ -1,6 +1,13 @@
+-- Player Information
 local player: Player = game:GetService("Players").LocalPlayer
 repeat task.wait() until player.Character
 local character = player.Character
 local humanoid = character:FindFirstChild("Humanoid")
 
-print(humanoid.RootPart.Position)
+-- Setup
+
+
+
+-- Options
+
+getgenv.OpenTraitRerollGUI = false
--- loader.lua
+++ loader.lua
@@ -1,8 +1,11 @@
 local player: Player = game:GetService("Players").LocalPlayer
 
+if isfolder("RocketHub") ~= true then
+    makefolder("RocketHub")
+end
+
 if game.PlaceId == 8304191830 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/KIunk/rockethub/main/AnimeAdventures.lua"))()
-
 else
     player:Kick("This game is currently not supported.")
 end
